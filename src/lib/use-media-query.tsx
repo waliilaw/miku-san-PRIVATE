dca29216c24b9655659ad588e3ca0b43488bea91
Wali <humaidwali20@gmail.com>
2025-03-07 23:17:18 +0530

    Create use-media-query.tsx

---
+++ src/lib/use-media-query.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useState, useEffect } from "react"
+
+export function useMediaQuery(query: string): boolean {
+  const [matches, setMatches] = useState(false)
+
+  useEffect(() => {
+    const media = window.matchMedia(query)
+
+    // Update the state initially
+    setMatches(media.matches)
+
+    // Set up the listener
+    const listener = () => setMatches(media.matches)
+    media.addEventListener("change", listener)
+
+    // Clean up
+    return () => media.removeEventListener("change", listener)
+  }, [query])
+
+  return matches
+}
+
